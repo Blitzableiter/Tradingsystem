0f84828a9be86acf513f569ed13ba2016c5a61f0
Max Rumford <max.rumford@gmx.net>
2020-05-06 20:23:56 +0200

    test stub

---
+++ Tradingsystem/src/test/java/de/rumford/tradingsystem/SubSystemTest.java
@@ -0,0 +1,34 @@
+package de.rumford.tradingsystem;
+
+import org.junit.jupiter.api.*;
+
+class SubSystemTest {
+
+//	Rule r1 = RealRule.from(null, null, null, null, 0, 0);
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testSubSystem() {
+	}
+
+	@Test
+	void testAreRulesUnique() {
+//		fail("Not yet implemented");
+	}
+
+}
