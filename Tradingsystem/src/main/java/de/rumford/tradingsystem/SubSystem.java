8bdb5e45c116e39dae06078db0d47407bd6fa305
Max Rumford <max.rumford@gmx.net>
2020-05-06 19:54:02 +0200

    Rule subdivision

--- Tradingsystem/src/main/java/de/rumford/tradingsystem/SubSystem.java
+++ Tradingsystem/src/main/java/de/rumford/tradingsystem/SubSystem.java
@@ -3,6 +3,8 @@
  */
 package de.rumford.tradingsystem;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 /**
  * de.rumford.tradingsystem
  * 
@@ -21,39 +23,53 @@ public class SubSystem {
 	 * 
 	 */
 	public SubSystem(BaseValue baseValue, Rule[] rules, double capital) {
-		this.evaluateRules(rules);
+
+		evaluateRules(rules);
+
+		RuleContainer[] ruleContainers = putRulesIntoRuleContainers(rules);
+
+		RuleContainer instanceRules;
+		if (ruleContainers.length > 3) {
+			instanceRules = subdivideRules(ruleContainers);
+		} else {
+			instanceRules = RuleContainer.fromRuleContainers(ruleContainers);
+		}
+
+		this.setRuleContainers(instanceRules);
 
 		this.setBaseValue(baseValue);
 		this.setCapital(capital);
+		this.setDiversificationMultiplier(new DiversificationMultiplier(rules));
 	}
 
-	private class RuleContainer {
-		public RuleContainer(Rule[] rules, RuleContainer[] ruleContainers) {
+	private RuleContainer[] putRulesIntoRuleContainers(Rule[] rules) {
+		RuleContainer[] ruleContainers = {};
+		for (Rule rule : rules) {
+			ruleContainers = ArrayUtils.add(ruleContainers, RuleContainer.fromRule(rule));
+		}
+		return ruleContainers;
+	}
+
+	private static class RuleContainer {
+		private Rule[] rules;
+		private RuleContainer[] ruleContainers;
+
+		RuleContainer(Rule[] rules, RuleContainer[] ruleContainers) {
 			this.setRules(rules);
 			this.setRuleContainers(ruleContainers);
 		}
 
-		private Rule[] rules;
-		private RuleContainer[] ruleContainers;
-
-		public boolean hasRules() {
-			return this.getRules() != null;
+		public static RuleContainer fromRule(Rule rule) {
+			Rule[] ruleAsArray = { rule };
+			return new RuleContainer(ruleAsArray, null);
 		}
 
-		public boolean hasSubRules() {
-			return this.getRulesContainers() != null;
-		}
-
-		public Rule[] getRules() {
-			return rules;
+		public static RuleContainer fromRuleContainers(RuleContainer[] ruleContainers) {
+			return new RuleContainer(null, ruleContainers);
 		}
 
 		private void setRules(Rule[] rules) {
 			this.rules = rules;
-		}
-
-		public RuleContainer[] getRulesContainers() {
-			return ruleContainers;
 		}
 
 		private void setRuleContainers(RuleContainer[] ruleContainers) {
@@ -67,7 +83,7 @@ public class SubSystem {
 	 * @param rules {@code Rule} An array of rules to be check for uniqueness.
 	 * @return {@code boolean} True, if the rules are unique. False otherwise.
 	 */
-	public boolean areRulesUnique(Rule[] rules) {
+	public static boolean areRulesUnique(Rule[] rules) {
 		for (int i = 0; i < rules.length - 1; i++) {
 			if (rules[i].equals(rules[i + 1])) {
 				return false;
@@ -82,33 +98,61 @@ public class SubSystem {
 	 * @param rules
 	 * @return
 	 */
-	private boolean evaluateRules(Rule[] rules) {
+	private static void evaluateRules(Rule[] rules) {
 		if (rules == null)
 			throw new IllegalArgumentException("Given rules must not be null");
 		if (!areRulesUnique(rules))
 			throw new IllegalArgumentException("The given rules are not unique. Only unique rules can be used.");
-
-		// TODO Verpacken jeder einzelnen Rule in einen RuleContainer
-		// TODO Übergeben des Arrays von RuleContainer nach subdivideRules
-		RuleContainer instanceRules = subdivideRules(rules);
-
-		/* If rules are unique set them. */
-		this.setRules(instanceRules);
-
-		return false;
 	}
 
-	private RuleContainer subdivideRules(Rule[] rules) {
-		// TODO Baum von den Blättern her zu Containern à 3 zusammenfassen, dann Array
-		// der Container rekursiv eingeben.
-		RuleContainer returnRules = null;
-		if (rules.length <= 3)
-			return new RuleContainer(rules, null);
+	/**
+	 * Subdivides the given array of given RuleContainer by 3 into RuleContainers.
+	 * If the resulting array of RuleContainers is longer than 3
+	 * {@link #subdivideRules(RuleContainer[])} will be called recursively. If not,
+	 * these 3 or less RuleContainers are put into another RuleContainer which is
+	 * then returned to the caller.
+	 * 
+	 * @param rules {@code RuleContainer[]} The array of {@link RuleContainer} to be
+	 *              subdivided.
+	 * @return {@link RuleContainer} A RuleContainer containing the an array of
+	 *         RuleContainers, each containing three RuleContainer out of the given
+	 *         array of RuleContainers.
+	 */
+	private static RuleContainer subdivideRules(RuleContainer[] rules) {
+		/* Evaluate in multiples of three how many RuleContainer are passed */
+		int loopsize = rules.length / 3;
+		if (rules.length % 3 != 0)
+			loopsize += 1;
 
-		/* number of layers is at this point 2 or higher */
-		int numberOfLayersToGo = rules.length / 3 + 1;
+		RuleContainer[] ruleContainers = {};
+		for (int i = 1; i <= loopsize; i++) {
+			RuleContainer[] relevantRules = {};
+			/*
+			 * If the number of remaining RuleContainer is > 3 there will be another loop,
+			 * therefore the rules array must be cleaned of the RuleContainers taken out.
+			 */
+			if (rules.length > 3) {
+				System.arraycopy(rules, 0, relevantRules, 0, 3);
+				System.arraycopy(rules, 3, rules, 0, rules.length - 3);
+			} else {
+				System.arraycopy(rules, 0, relevantRules, 0, rules.length);
+			}
+			/*
+			 * Create a new RuleContainer to store the newly retrieved rules and add it to
+			 * the array of RuleContainer.
+			 */
+			RuleContainer tempRuleContainer = RuleContainer.fromRuleContainers(relevantRules);
+			ruleContainers = ArrayUtils.add(ruleContainers, tempRuleContainer);
+		}
 
-		return returnRules;
+		/*
+		 * If the array still consists of more than 3 elements another layer must be
+		 * added. Therefore subdivideRules() is called recursively.
+		 */
+		if (ruleContainers.length > 3)
+			return subdivideRules(ruleContainers);
+
+		return RuleContainer.fromRuleContainers(ruleContainers);
 	}
 
 	/**
@@ -134,14 +178,14 @@ public class SubSystem {
 	/**
 	 * @return rules SubSystem
 	 */
-	private RuleContainer getRules() {
+	private RuleContainer getRuleContainers() {
 		return ruleContainers;
 	}
 
 	/**
 	 * @param rules the rules to set
 	 */
-	private void setRules(RuleContainer ruleContainers) {
+	private void setRuleContainers(RuleContainer ruleContainers) {
 		this.ruleContainers = ruleContainers;
 	}
 
