93331ab9da2c69f8bc107d2b841a03167eaf2c4a
Max Rumford <max.rumford@gmx.net>
2020-05-10 18:22:50 +0200

    starting on clients

--- Tradingsystem/src/main/java/de/rumford/tradingsystem/Client.java
+++ Tradingsystem/src/main/java/de/rumford/tradingsystem/Client.java
@@ -1,6 +1,11 @@
 package de.rumford.tradingsystem;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 import de.rumford.tradingsystem.helper.CsvFormat;
 import de.rumford.tradingsystem.helper.DataSource;
@@ -11,16 +16,177 @@ public class Client {
 	private Client() {
 	}
 
-	public static void main(String[] args) throws IOException {
-		ValueDateTupel[] values = readValuesFromCsv("resources/DAX.csv", CsvFormat.EU);
-		ValueDateTupel[] shortIndexValues = readValuesFromCsv("resources/DAX_short.csv", CsvFormat.EU);
-		BaseValue baseValue = new BaseValue("DAX", values, shortIndexValues);
+	static boolean tryAgainFlag = true;
+	/* Setup */
+	static final String WORKING_DIR = Path.of("src", "test", "resources").toString();
+	static final BufferedReader IN = new BufferedReader(new InputStreamReader(System.in));
+	static BaseValue baseValue;
+	static Registrar registrar;
 
-		ValueDateTupel[][] notAligned = { values, shortIndexValues };
-		ValueDateTupel[][] aligned = ValueDateTupel.alignDates(notAligned);
+	public static void main(String[] args) throws IOException {
+
+		baseValueSetup();
+
+//		ruleRegistration();
+
+		ruleSetup();
+
+		System.out.println("Goodbye!");
+	}
+
+	private static void ruleRegistration() throws IOException {
+		registrar = new Registrar();
+		boolean addRulesFlag;
+		tryAgainFlag = true;
+
+		while (tryAgainFlag) {
+			try {
+				tryAgainFlag = false;
+				String input;
+
+				/* Read in class names */
+				addRulesFlag = ynQuestion("Do you want to register additional types of rules?", "N");
+
+				if (addRulesFlag) {
+					tryAgainFlag = true;
+					input = askForInput("Which rule do you want to register?");
+					@SuppressWarnings("unchecked")
+					Class<? extends Rule> clazz = (Class<? extends Rule>) Class.forName(input);
+					registrar.registerRule(clazz);
+					System.out.println("Rule successfully registered.");
+				}
+			} catch (ClassNotFoundException e) {
+				System.out.println("The class " + e.getMessage()
+						+ " could not be found. Please provide the fully qualified name.");
+				tryAgainFlag = tryAgainQuestion();
+			} catch (Exception e) {
+				System.out.println("That didn't work. An error was thrown: " + e.getMessage());
+				tryAgainFlag = tryAgainQuestion();
+			}
+		}
+	}
+
+	private static boolean tryAgainQuestion() throws IOException {
+		return ynQuestion("Do you want to try again?", "N");
+	}
+
+	private static void ruleSetup() throws IOException {
+		tryAgainFlag = true;
+		Rule[] rules = {};
+
+		while (tryAgainFlag) {
+			try {
+				String input;
+
+				/* Read in file names */
+				input = askForInput("Which rule do you want to use?");
+				@SuppressWarnings("unchecked")
+				Class<? extends Rule> clazz = (Class<? extends Rule>) Class.forName(input);
+
+				tryAgainFlag = false;
+			} catch (ClassNotFoundException e) {
+				System.out.println("The class " + e.getMessage()
+						+ " could not be found. Please provide the fully qualified name.");
+				tryAgainFlag = tryAgainQuestion();
+			} catch (Exception e) {
+				System.out.println("That didn't work. An error was thrown: " + e.getMessage());
+				tryAgainFlag = tryAgainQuestion();
+			}
+		}
+	}
+
+	private static void baseValueSetup() throws IOException {
+		String nameInput = askForInput("What name should this base value have?");
+
+		while (tryAgainFlag) {
+			try {
+				String input;
+
+				/* Read in file names */
+				input = askForInput("Which data source do you want to use?");
+				ValueDateTupel[] values = readValuesFromCsv(Path.of(WORKING_DIR, input).toString(), CsvFormat.EU);
+
+				if (ynQuestion("Do you want to provide short index values?", "N")) {
+					input = askForInput("Which data source do you want to use?");
+					ValueDateTupel[] shortIndexValues = readValuesFromCsv(Path.of(WORKING_DIR, input).toString(),
+							CsvFormat.EU);
+
+					ValueDateTupel[][] notAligned = { values, shortIndexValues };
+					ValueDateTupel[][] aligned = ValueDateTupel.alignDates(notAligned);
+
+					/* Construct BaseValue */
+					baseValue = new BaseValue(nameInput, aligned[0], aligned[1]);
+				} else {
+					baseValue = new BaseValue(nameInput, values);
+				}
+
+				System.out.println("Data sources succesfully read.");
+
+				tryAgainFlag = false;
+			} catch (Exception e) {
+				System.out.println("That didn't work. An error was thrown: " + e.getMessage());
+				tryAgainFlag = tryAgainQuestion();
+			}
+		}
+		tryAgainFlag = true;
+	}
+
+	private static String askForInput(String question) throws IOException {
+		System.out.print(question + " ");
+		return IN.readLine();
+	}
+
+	private static boolean ynQuestion(String question, String defaultValue) throws IOException {
+		StringBuilder questionSB = new StringBuilder();
+		questionSB.append(question);
+		questionSB.append(" ");
+		String options = (defaultValue.toLowerCase().contentEquals("y") ? "Y/n" : "y/N");
+		questionSB.append(options);
+		questionSB.append(" > ");
+
+		boolean unRecognizedFlag = false;
+		while (!unRecognizedFlag) {
+			System.out.println(questionSB.toString());
+			String input = IN.readLine();
+			if (input.toLowerCase().contentEquals("y")) {
+				return true;
+			} else if (input.contentEquals("") || input.toLowerCase().contentEquals("n")) {
+				return false;
+			} else {
+				System.out.println("Option not recognized. Try again.");
+				unRecognizedFlag = true;
+			}
+		}
+		return false;
+
 	}
 
 	private static ValueDateTupel[] readValuesFromCsv(String path, CsvFormat csvFormat) throws IOException {
 		return DataSource.getDataFromCsv(path, csvFormat);
+	}
+
+	static class Registrar {
+
+		List<Class<? extends Rule>> registeredRules;
+
+		public Registrar() {
+			registeredRules = new ArrayList<>();
+			registeredRules.add(VolatilityDifference.class);
+			registeredRules.add(EWMAC.class);
+		}
+
+		public void registerRule(Class<? extends Rule> ruleClass) {
+			List<Class<? extends Rule>> currentRules = this.getRegisteredRules();
+			currentRules.add(ruleClass);
+			this.setRegisteredRules(currentRules);
+		}
+
+		public List<Class<? extends Rule>> getRegisteredRules() {
+			return this.registeredRules;
+		}
+
+		private void setRegisteredRules(List<Class<? extends Rule>> registeredRules) {
+			this.registeredRules = registeredRules;
+		}
 	}
 }
---
+++ Tradingsystem/src/main/java/de/rumford/tradingsystem/ProgrammersClient.java
@@ -0,0 +1,91 @@
+package de.rumford.tradingsystem;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+
+import de.rumford.tradingsystem.helper.CsvFormat;
+import de.rumford.tradingsystem.helper.DataSource;
+import de.rumford.tradingsystem.helper.ValueDateTupel;
+
+public class ProgrammersClient {
+	static final String WORKING_DIR = Path.of("src", "test", "resources").toString();
+
+	static final double CAPITAL = 10000;
+	static final String BASE_VALUE_NAME = "DAX";
+	static final String BASE_VALUE_FILE_NAME = Path.of(WORKING_DIR, "DAX - mod.csv").toString();
+	static final String SHORT_INDEX_VALUE_FILE_NAME = Path.of(WORKING_DIR, "DAX_short - mod.csv").toString();
+
+	static final LocalDateTime START_OF_REFERENCE_WINDOW = LocalDateTime.of(2019, 1, 2, 22, 0);
+	static final LocalDateTime END_OF_REFERENCE_WINDOW = LocalDateTime.of(2019, 12, 30, 22, 0);
+	static final double BASE_SCALE = 10;
+
+	static final int LOOKBACK_WINDOW_2 = 2;
+	static final int LOOKBACK_WINDOW_4 = 4;
+	static final int LOOKBACK_WINDOW_8 = 8;
+
+	static BaseValue baseValue;
+
+	static VolatilityDifference volDif2;
+	static VolatilityDifference volDif4;
+	static VolatilityDifference volDif8;
+	static VolatilityDifference volDifTop;
+
+	static EWMAC ewmacShort;
+	static EWMAC ewmacMiddle;
+	static EWMAC ewmacLong;
+	static EWMAC ewmacTop;
+
+	static SubSystem subSystem;
+
+	private ProgrammersClient() {
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		ValueDateTupel[] shortIndexValues = DataSource.getDataFromCsv(SHORT_INDEX_VALUE_FILE_NAME, CsvFormat.EU);
+		baseValue = new BaseValue(BASE_VALUE_NAME, DataSource.getDataFromCsv(BASE_VALUE_FILE_NAME, CsvFormat.EU),
+				shortIndexValues);
+		System.out.println("after base value");
+
+		volDif2 = new VolatilityDifference(baseValue, null, START_OF_REFERENCE_WINDOW, END_OF_REFERENCE_WINDOW,
+				LOOKBACK_WINDOW_2, BASE_SCALE);
+		System.out.println("after voldif2");
+		volDif4 = new VolatilityDifference(baseValue, null, START_OF_REFERENCE_WINDOW, END_OF_REFERENCE_WINDOW,
+				LOOKBACK_WINDOW_4, BASE_SCALE);
+		System.out.println("after voldif4");
+		volDif8 = new VolatilityDifference(baseValue, null, START_OF_REFERENCE_WINDOW, END_OF_REFERENCE_WINDOW,
+				LOOKBACK_WINDOW_8, BASE_SCALE);
+		System.out.println("after voldif8");
+		VolatilityDifference[] volDifVariations = { //
+				volDif2, //
+				volDif4, //
+				volDif8 };
+		volDifTop = new VolatilityDifference(baseValue, volDifVariations, START_OF_REFERENCE_WINDOW,
+				END_OF_REFERENCE_WINDOW, 8, BASE_SCALE);
+		System.out.println("after voldiftop");
+
+		ewmacShort = new EWMAC(baseValue, null, START_OF_REFERENCE_WINDOW, END_OF_REFERENCE_WINDOW, 8, 2, BASE_SCALE);
+		System.out.println("after ewmac short");
+		ewmacMiddle = new EWMAC(baseValue, null, START_OF_REFERENCE_WINDOW, END_OF_REFERENCE_WINDOW, 16, 4, BASE_SCALE);
+		System.out.println("after ewmac middle");
+		ewmacLong = new EWMAC(baseValue, null, START_OF_REFERENCE_WINDOW, END_OF_REFERENCE_WINDOW, 32, 8, BASE_SCALE);
+		System.out.println("after ewmac long");
+		EWMAC[] ewmacVariations = { //
+				ewmacShort, //
+				ewmacMiddle, //
+				ewmacLong };
+		ewmacTop = new EWMAC(baseValue, ewmacVariations, START_OF_REFERENCE_WINDOW, END_OF_REFERENCE_WINDOW, 0, 0,
+				BASE_SCALE);
+
+		System.out.println(ValueDateTupel.getElement(baseValue.getValues(), END_OF_REFERENCE_WINDOW));
+		System.out.println(ewmacTop.getForecast(END_OF_REFERENCE_WINDOW));
+		System.out.println(volDifTop.getForecast(END_OF_REFERENCE_WINDOW));
+
+		Rule[] rules = { volDifTop, ewmacTop };
+
+		subSystem = new SubSystem(baseValue, rules, CAPITAL);
+
+	}
+
+}
