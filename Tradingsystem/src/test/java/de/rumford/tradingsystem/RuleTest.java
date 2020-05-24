f6633560b134ae6e6426209fa634f2fe43475461
Max Rumford <max.rumford@gmx.net>
2020-05-24 09:15:23 +0200

    removing validator tests

--- Tradingsystem/src/test/java/de/rumford/tradingsystem/RuleTest.java
+++ Tradingsystem/src/test/java/de/rumford/tradingsystem/RuleTest.java
@@ -12,7 +12,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import de.rumford.tradingsystem.helper.*;
+import de.rumford.tradingsystem.helper.BaseValueFactory;
+import de.rumford.tradingsystem.helper.GeneratedCode;
+import de.rumford.tradingsystem.helper.ValueDateTupel;
 
 /**
  * Test class for {@link Rule}.
@@ -524,80 +526,6 @@ class RuleTest {
 				realRule.getVariations()[2].getWeight() };
 
 		assertArrayEquals(expectedValue, actualValue, "Weights for 3 equal variations are not correctly calculated");
-	}
-
-	/**
-	 * Test method for {@link Validator#validateCorrelations(double[])}.
-	 */
-	@Test
-	void testValidateCorrelations_arrayNull() {
-		String expectedMessage = "Correlations array must not be null";
-		double[] correlations = null;
-
-		Exception thrown = assertThrows(IllegalArgumentException.class,
-				() -> Util.calculateWeightsForThreeCorrelations(correlations), "Array of null is not properly handled");
-
-		assertEquals(expectedMessage, thrown.getMessage(), MESSAGE_INCORRECT_EXCEPTION_MESSAGE);
-	}
-
-	/**
-	 * Test method for {@link Validator#validateCorrelations(double[])}.
-	 */
-	@Test
-	void testValidateCorrelations_arrayOfLengthNotThree() {
-		String expectedMessage = "There must be exactly three correlation values in the given array";
-		double[] correlations = { 0, 0 };
-
-		Exception thrown = assertThrows(IllegalArgumentException.class,
-				() -> Util.calculateWeightsForThreeCorrelations(correlations),
-				"Array of length != 3 is not properly handled");
-
-		assertEquals(expectedMessage, thrown.getMessage(), MESSAGE_INCORRECT_EXCEPTION_MESSAGE);
-	}
-
-	/**
-	 * Test method for {@link Validator#validateCorrelations(double[])}.
-	 */
-	@Test
-	void testValidateCorrelations_arrayContainsNan() {
-		String expectedMessage = "NaN-values are not allowed. Correlation at position 1 is NaN.";
-		double[] correlations = { 0, Double.NaN, 0 };
-
-		Exception thrown = assertThrows(IllegalArgumentException.class,
-				() -> Util.calculateWeightsForThreeCorrelations(correlations),
-				"Array containing Double.NaN is not properly handled");
-
-		assertEquals(expectedMessage, thrown.getMessage(), MESSAGE_INCORRECT_EXCEPTION_MESSAGE);
-	}
-
-	/**
-	 * Test method for {@link Validator#validateCorrelations(double[])}.
-	 */
-	@Test
-	void testValidateCorrelations_arrayContainsValueGreaterThan1() {
-		String expectedMessage = "Correlation at position 1 is greater than 1";
-		double[] correlations = { 0, 2, 0 };
-
-		Exception thrown = assertThrows(IllegalArgumentException.class,
-				() -> Util.calculateWeightsForThreeCorrelations(correlations),
-				"Array containing values greater than 1 is not properly handled");
-
-		assertEquals(expectedMessage, thrown.getMessage(), MESSAGE_INCORRECT_EXCEPTION_MESSAGE);
-	}
-
-	/**
-	 * Test method for {@link Validator#validateCorrelations(double[])}.
-	 */
-	@Test
-	void testValidateCorrelations_arrayContainsValueLessThanNegative1() {
-		String expectedMessage = "Correlation at position 1 is less than -1";
-		double[] correlations = { 0, -2, 0 };
-
-		Exception thrown = assertThrows(IllegalArgumentException.class,
-				() -> Util.calculateWeightsForThreeCorrelations(correlations),
-				"Array containing values less than -1 is not properly handled");
-
-		assertEquals(expectedMessage, thrown.getMessage(), MESSAGE_INCORRECT_EXCEPTION_MESSAGE);
 	}
 
 	/**
