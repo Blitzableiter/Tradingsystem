99aa09cbb13138d02d2d7bbbad6418be7ff2d135
Max Rumford <max.rumford@gmx.net>
2020-06-11 18:30:39 +0200

    enhancing example client + ignoring for test coverage + javadoc.jar

---
+++ Tradingsystem/src/main/java/de/rumford/tradingsystem/helper/JacocoIgnoreGenerated.java
@@ -0,0 +1,13 @@
+package de.rumford.tradingsystem.helper;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ * Indicates that a method declaration is to be ignored by JaCoCo test
+ * coverage calculation.
+ */
+@Target(ElementType.TYPE)
+public @interface JacocoIgnoreGenerated {
+
+}
